c3a23ede27412a56052fb6ed5985dd460b348932
roc <granadaroc96@gmail.com>
2017-04-20 00:51:42 +0200

    Web completely integrated with django. Same shit now dinamic

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.home, name='home'),
+    url(r'^entries/(?P<id>.*)/', views.entries, name='entries'), #([0-9]{4})/([0-9]{2})/ add data to url????
+    url(r'^contact/', views.contact, name='contact'),
+    url(r'^about-me/', views.about, name='about')
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,16 +4,24 @@ from .models import Entry
 
 # Create your views here.
 
-def index(request):
-    entries = Entry.objects.all()
+def home(request):
+    entries = Entry.objects.order_by('creation_date')
     context = {
         'entries':entries,
     }
-    return render(request, 'index.html', context)
+    return render(request, 'home.html', context)
 
 def entries(request,id):
-    entry = Entry.objects.filter(identifier=id)
+    entry = Entry.objects.get(identifier=id)
     context = {
         'entry':entry,
     }
     return render(request,'entry.html',context)
+
+def contact(request):
+    context = {}
+    return render(request,'contact.html',context)
+
+def about(request):
+    context = {}
+    return render(request,'about.html',context)
